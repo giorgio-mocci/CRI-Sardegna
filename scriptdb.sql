65afa88c511b2327876d2df6ef3e414b3b878b24
Eimitfi <macarena7799@gmail.com>
2020-09-25 00:17:12 +0200

    aggiunta trigger al db

--- scriptdb.sql
+++ scriptdb.sql
@@ -9,7 +9,7 @@ cognome VARCHAR(30) NOT NULL,
 mail VARCHAR(40) UNIQUE NOT NULL,
 telefono CHAR(10) UNIQUE NOT NULL,
 password TEXT NOT NULL,
-whitelist SMALLINT NOT NULL CHECK(whitelist=1),
+whitelist SMALLINT NOT NULL,
 fk_comitato VARCHAR(22) NOT NULL,
 DRAE SMALLINT NOT NULL,
 responsabile SMALLINT NOT NULL,
@@ -60,3 +60,26 @@ FOREIGN KEY (fk_attr) REFERENCES attrezzature_mezzi(nome) ON UPDATE cascade ON D
 FOREIGN KEY (fk_comitato) REFERENCES comitati(nome) ON UPDATE cascade ON DELETE no action,
 PRIMARY KEY(fk_attr, fk_comitato)
 );
+
+do $$
+begin
+
+IF (NOT EXISTS (SELECT * FROM pg_trigger WHERE tgname='DRAE')) THEN
+
+CREATE FUNCTION no_DRAE() RETURNS trigger AS $no_DRAE$
+    BEGIN
+        IF (NOT EXISTS(select * FROM volontari V WHERE NEW.fk_DRAE=V.cf AND V.DRAE=1)) THEN
+            RAISE EXCEPTION 'il creatore dell evento non è un DRAE';
+		RETURN NULL;
+	ELSE
+		RETURN NEW;
+        END IF;
+    END;
+$no_DRAE$ LANGUAGE plpgsql;
+
+CREATE TRIGGER NO_DRAE BEFORE INSERT OR UPDATE ON eventi
+    FOR EACH ROW EXECUTE PROCEDURE no_DRAE();
+
+END IF;
+end
+$$
